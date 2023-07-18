7ba54e36f07085f36596baabe6ea4353944a8512
Nurbek7182 <nurbekkadyr7182@gmail.com>
2023-07-18 16:47:14 +0600

    [main] d3 changes

--- src/app/d3-practice/d3-practice.js
+++ src/app/d3-practice/d3-practice.js
@@ -1,17 +1,24 @@
-import * as d3 from "d3";
-
-let w = 500;
-let h = 100;
-let padding = 2;
-let dataset = [5, 10, 15, 20, 25];
-let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
-
-svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("height", 100)
-  .attr("width", 20);
+// let w = 500;
+// let h = 100;
+// let padding = 2;
+// let dataset = [5, 10, 15, 20, 25];
+// let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+//
+// svg
+//   .selectAll("rect")
+//   .data(dataset)
+//   .enter()
+//   .append("rect")
+//   .attr("x", 0)
+//   .attr("y", 0)
+//   .attr("height", 100)
+//   .attr("width", 20);
+//
+// d3.select("body")
+//   .append("svg")
+//   .append("rect")
+//   .attr("width", 50)
+//   .attr("height", 200)
+//   .style("fill", "red");
+//
+// console.log("hello d3.js world");
