342135d54b93eead4e3ec500614047ae35932b25
Nurbek7182 <nurbekkadyr7182@gmail.com>
2023-07-21 18:03:38 +0600

    [main] small changes

--- src/app/d3-practice/d3-practice-v2.js
+++ src/app/d3-practice/d3-practice-v2.js
@@ -6,11 +6,28 @@ svg.setAttribute("width", width);
 svg.setAttribute("height", height);
 document.body.appendChild(svg);
 
+const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+mask.setAttribute("id", "circle-mask");
+svg.appendChild(mask);
+
+const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+circle.setAttribute("r", 600);
+circle.setAttribute("cx", width / 2);
+circle.setAttribute("cy", height / 2);
+circle.setAttribute("fill", "black");
+mask.appendChild(circle);
+
+let rectV = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
 for (let i = 0; i < 20; i++) {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", i * 50);
-  rect.setAttribute("width", 10);
-  rect.setAttribute("height", height);
-  rect.setAttribute("fill", "black");
-  svg.appendChild(rect);
+  rectV.setAttribute("x", i * 100);
+  rectV.setAttribute("width", 10);
+  rectV.setAttribute("height", height);
+  rectV.setAttribute("fill", "black");
+  svg.appendChild(rectV);
 }
+
+rectV.setAttribute("mask", "url(#circle-mask)");
+
+const rectH = document.createElementNS("http://www.w3.org/2000/svg", "rect");
