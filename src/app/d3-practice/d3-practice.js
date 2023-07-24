e36fd553ce873151e711bb6d29bc9ae123fbb30a
Nurbek7182 <nurbekkadyr7182@gmail.com>
2023-07-24 18:03:18 +0600

    [main] changing d3-practice

---
+++ src/app/d3-practice/d3-practice.js
@@ -0,0 +1,48 @@
+let w = 300;
+let h = 400;
+let padding = 2;
+let dataset = [5, 10, 15, 20, 25, 11, 22, 32, 14, 15];
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", 1920)
+  .attr("width", 1080);
+
+function colorPicker(v) {
+  if (v <= 20) {
+    return "#666666";
+  } else if (v > 20) {
+    return "#fff033";
+  }
+}
+
+svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("x", function (d, i) {
+    return (i * w) / dataset.length;
+  })
+  .attr("y", function (d) {
+    return h - d * 5;
+  })
+  .attr("width", w / dataset.length - padding)
+  .attr("height", function (d) {
+    return d * 5;
+  })
+  .attr("fill", function (d) {
+    return colorPicker(d);
+  });
+
+svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function (d) {
+    return d;
+  })
+  .attr({
+    textAnchor: "middle",
+  });
